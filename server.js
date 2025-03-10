2d41fd5796fdfc456b979a965534018cea9f0924
Arron Robinson <arron_robinson50@hotmail.com>
2025-03-10 13:16:23 +0100

    favorite pagina route

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const uri = `mongodb+srv://${process.env.DATA_USERNAME}:${process.env.DATA_PW}@${process.env.DATA_HOST}/${process.env.DATA_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 const PORT = 3000;
+const SECRET_KEY = process.env.JWT_SECRET 
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,7 +40,8 @@ app
     .get('/', onindex)
     .get('/signup', onsignup)
     .get('/login', onlogin)
-    .get('/explore', onexplore);
+    .get('/explore', onexplore)
+    .get('/favorite', onfavorite);
 
 function onexplore(req, res) {
     res.render('explore', { title: 'Explore Page' });
@@ -55,6 +59,10 @@ function onlogin(req, res) {
     res.render('login', { title: 'Log In Page' });
 }
 
+function onfavorite(req, res) {
+    res.render('favorite', { title: 'Favorite Page' });
+}
+
 app.post("/signup", async (req, res) => {
     const { username, email, password } = req.body;
 
@@ -69,10 +77,35 @@ app.post("/signup", async (req, res) => {
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
 
-        res.redirect("/explore");
+        res.redirect("/login");
     } catch (error) {
         res.status(500).json({ message: "Error signing up" });
     }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: "User not found"});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message: "Invalid credentials"});
+
+        const token = jwt.sign({ userId: user._id, username: user.username}, SECRET_KEY, {expiresIn: "7d"});
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        res.redirect("/explore");
+    } catch (error) {
+        res.status(500).json({message: "error logging in"});
+    }
+    
 });
 
 app.listen(PORT, () => {
